c60e294860207eb0582c26856f315deea3b59da5
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-08-18 16:41:46 +0100

    a simple guessing game

--- loop.js
+++ loop.js
@@ -1,42 +1,42 @@
-// for (let index = 12; index >=1; index--) {
-//     console.log(`${index}x ${index} = ${index * index}`)
-// }
-// let listOfNumbers = [5,5,5,5,5,5,5]
-// let answer = 0
-// for(let i = 0;i < listOfNumbers.length; i++ ){
-//     answer += listOfNumbers[i]
-// }
-// console.log(answer)
-// // A CODE TO CALCULATE THE AVEARGE OF THE TOTAL SCORE OF STUDENTS OF A COURSE
-// let studentList = [
-//     {
-//         name : 'obi okafor',
-//         score : 56
-//     },
-//     {
-//         name : 'benjamin ariwodo',
-//         score : 70
-//     },
-//     {
-//         name : 'richard Eze',
-//         score : 75
-//     },
-//     {
-//         name : 'otete prince ',
-//         score : 60
-//     },
-//     {
-//         name : 'Ojingwa Daniel',
-//         score : 80
-//     }
-// ]
-// let avgScore = 0
-// for (let i = 0; i < studentList.length; i++) {
-//     let student = studentList[i]
-//     avgScore += student.score
-// }
-// console.log(avgScore/studentList.length)
-// A CODE TO CALCULATE A TOTAL SCORE OF NUMBERS IN A GAMEBOARD
+for (let index = 12; index >=1; index--) {
+    console.log(`${index}x ${index} = ${index * index}`)
+}
+let listOfNumbers = [5,5,5,5,5,5,5]
+let answer = 0
+for(let i = 0;i < listOfNumbers.length; i++ ){
+    answer += listOfNumbers[i]
+}
+console.log(answer)
+// A CODE TO CALCULATE THE AVEARGE OF THE TOTAL SCORE OF STUDENTS OF A COURSE
+let studentList = [
+    {
+        name : 'obi okafor',
+        score : 56
+    },
+    {
+        name : 'benjamin ariwodo',
+        score : 70
+    },
+    {
+        name : 'richard Eze',
+        score : 75
+    },
+    {
+        name : 'otete prince ',
+        score : 60
+    },
+    {
+        name : 'Ojingwa Daniel',
+        score : 80
+    }
+]
+let avgScore = 0
+for (let i = 0; i < studentList.length; i++) {
+    let student = studentList[i]
+    avgScore += student.score
+}
+console.log(avgScore/studentList.length)
+//A CODE TO CALCULATE A TOTAL SCORE OF NUMBERS IN A GAMEBOARD
 let gameBoard = [
     [12,34,25,7,45],
     [15,23,37,5,37],
@@ -52,3 +52,14 @@ for(let i =0; i < gameBoard.length; i++){
     }
 }
 console.log(totalScore)
+// CREATING  A GUESSING GAME
+const number = Math.floor(Math.random() * 10)
+let GuessedNumber = Math.floor(Math.random() * 10)
+while (GuessedNumber !== number) {
+    console.log(`Number : ${number} \n GuessedNumber : ${GuessedNumber}`)
+    GuessedNumber = Math.floor(Math.random() * 10)
+}
+console.log(`Number : ${number} \n GuessedNumber : ${GuessedNumber}`)
+console.log('CONGRATS YOU WON')
+
+
