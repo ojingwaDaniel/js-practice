1dceb8b9bdb733f890cf2ad06167a4374dbd18d1
Ojingwa Daniel <79636701+ojingwaDaniel@users.noreply.github.com>
2022-08-18 08:35:29 +0100

    loop layout

---
+++ loop.js
@@ -0,0 +1,10 @@
+// for (let index = 1; index <=12; index++) {
+//     console.log(`${index}x ${index} = ${index * index}`)
+// }
+let listOfNumbers = [5,5,5,5,5,5,5]
+let answer = 0
+for(let i = 0;i < listOfNumbers.length; i++ ){
+    answer += listOfNumbers[i]
+}
+console.log(answer)
+
